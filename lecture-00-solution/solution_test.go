376a53d3676d15d77bf42f16f24875582e61cf5c
Nick Andrejeu <andrejeu@gmail.com>
2022-02-11 22:08:04 +0400

    add emoji import in test

--- lecture-00-solution/solution_test.go
+++ lecture-00-solution/solution_test.go
@@ -1,10 +1,14 @@
 package solution
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/kyokomi/emoji"
+)
 
 func TestGetMessage(t *testing.T) {
 	got := GetMessage()
-	want := "Hello :world_map:"
+	want := emoji.Sprint("Hello :world_map:")
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
